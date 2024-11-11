b841b69ef0dfde6aac8c62a025f3974fd5ab072f
Miko <110693261+mikomikotaishi@users.noreply.github.com>
2024-11-10 20:07:56 -0500

    Replace if/else with match

--- src/main.py
+++ src/main.py
@@ -14,27 +14,27 @@ def main() -> int:
     """
     try:
         manual_input: bool = False
-        if (len(sys.argv) == 1):
-            manual_input = True
-            input_file: str = input("Enter the name of the Excel file: ")
-            file_format: str = input("Enter the format of the Excel file (A or B): ")
-            needs_output: str = input("Do you want to specify an output file? (y/n): ")
-            output: bool = True if needs_output.lower() == 'y' else False
-            if output:
-                output_file: str = input("Enter the name of the output file: ")
-        else:            
-            if (len(sys.argv) == 2):
+        match len(sys.argv):
+            case 1:
+                manual_input = True
+                input_file: str = input("Enter the name of the Excel file: ")
+                file_format: str = input("Enter the format of the Excel file (A or B): ")
+                needs_output: str = input("Do you want to specify an output file? (y/n): ")
+                output: bool = True if needs_output.lower() == 'y' else False
+                if output:
+                    output_file: str = input("Enter the name of the output file: ")
+            case 2:
                 raise ValueError("No file format specified")
-            elif (len(sys.argv) == 4):
-                raise ValueError("No output file specified")
-            elif (len(sys.argv) > 5):
-                raise ValueError("Too many arguments")
-            else:
+            case 3:
                 parser: argparse.ArgumentParser  = argparse.ArgumentParser(description = "Read an Excel file")
                 parser.add_argument("file_name", type = str, help = "Name of the Excel file")
                 parser.add_argument("file_format", choices = ['A', 'B'], help = "Format of Excel file (A or B)")
                 parser.add_argument("-o", "--output", type = str, help = "Name of the output file (optional)")
                 args: argparse.Namespace = parser.parse_args()
+            case 4:
+                raise ValueError("No output file specified")
+            case _:
+                raise ValueError("Too many arguments")
 
         processed_data: pd.DataFrame = process_excel((input_file if manual_input else args.file_name), (file_format if manual_input else args.file_format))
 
